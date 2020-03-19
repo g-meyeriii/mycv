eb0d831bf4e849fed00aaa5d84ae1b109c74be7a
max-student <max-student@maxtrain.com>
2020-03-19 16:52:12 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mycv';
+  personal: string = "George|1234 Main St Florence KY 23456|513-333-4444";
+  h3Color: string= "blue";
+  mainColor: string= "green";
+  tech: string= "Git/GitHub SQL C# EF MVC WebApi Html Css JavaScript jQuery Typescript Anglar";
+  workHist1:string="";
+  workHist2:string="";
+  education1:string="";
+  education2:string="";
 }
